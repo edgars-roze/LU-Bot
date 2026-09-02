2f70e985ef8cac545cda297fb906a93f32eea931
edgars-roze <klopojed.son@gmail.com>
2026-09-02 10:55:46 +0300

    Messages

--- api/webhook.py
+++ api/webhook.py
@@ -4,4 +4,14 @@ import requests
 BOT_TOKEN = os.environ['BOT_TOKEN']
 
 def handler(request):
-	return request
+	message = request.get_json().get('message')
+	chat_id = message['chat']['id']
+	text = message['text']
+
+	requests.post(
+		f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+		json={
+			"chat_id": chat_id,
+			"text": f"You said: {text}"
+		}
+	)
