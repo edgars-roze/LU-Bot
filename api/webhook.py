22d14e9194004f878879eb93da950cd11e65f6f2
edgars-roze <klopojed.son@gmail.com>
2026-09-02 13:18:04 +0300

    First command

--- api/webhook.py
+++ api/webhook.py
@@ -8,12 +8,16 @@ BOT_TOKEN = os.environ['BOT_TOKEN']
 bot = telegram.Bot(BOT_TOKEN)
 app = telegram.Application.builder().token(BOT_TOKEN).build()
 
+async def help(update: telegram.Update, context):
+	await update.message.reply_text(
+		"No help"
+	)
+
+app.add_handler(telegram.CommandHandler('help', help))
+
 @api.post('/api/webhook')
 async def webhook(request: fastapi.Request):
 	json = await request.json()
-	update = telegram.Update.de_json(json, bot)
-
-	if update.message and update.message.text:
-		await update.message.reply_text(update.message.text)
+	await app.process_update(telegram.Update.de_json(json, bot))
 
 	return 'OK'
