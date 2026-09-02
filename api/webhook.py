5cd2aafad17c39b0e732d55c2fa39e1add5e2200
edgars-roze <klopojed.son@gmail.com>
2026-09-02 12:36:40 +0300

    Attempt to use requests_async

--- api/webhook.py
+++ api/webhook.py
@@ -1,5 +1,5 @@
 import os
-import requests
+import requests_async
 from fastapi import FastAPI, Request
 
 app = FastAPI()
@@ -8,16 +8,16 @@ BOT_TOKEN = os.environ['BOT_TOKEN']
 
 @app.post('/api/webhook')
 async def webhook(request: Request):
-	update = await request.get_json()
+	update = await request.json()
 	message = update.get('message')
 
 	if not message:
-		return 'OK', 200
+		return 'OK'
 
 	chat_id = message['chat']['id']
 	text = message.get('text', '')
 
-	requests.post(
+	requests_async.post(
         f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage',
 		json={
 			'chat_id': chat_id,
@@ -25,4 +25,4 @@ async def webhook(request: Request):
 		}
 	)
 
-	return 'OK', 200
+	return 'OK'
